a730c72548809ed233f1a9cab87aca5d75c534b6
Michael Mercier <michael.mercier@ryax.tech>
2024-03-18 18:18:36 +0100

    Add firewall rules for submariner

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ module "cluster_1" {
   service_cidr_block     = "10.43.0.0/16"
 }
 
+
 module "cluster_2" {
   source = "github.com/RyaxTech/terraform-aws-kubeadm"
 
@@ -27,4 +28,32 @@ module "cluster_2" {
   num_workers            = 1
   pod_network_cidr_block = "10.0.0.0/16"
   service_cidr_block     = "10.1.0.0/16"
+}
+
+resource "aws_security_group" "submariner" {
+  name        = "inter-cluster-submariner"
+  description = "Allow Submariner port"
+  vpc_id      = module.cluster_1.vpc_id
+  ingress {
+    protocol  = "udp"
+    from_port = 4500
+    to_port   = 4500
+    cidr_blocks = ["172.31.0.0/16"]
+    # concat(
+    #   [for node in module.cluster_1.cluster_nodes : "${node.public_ip}/32"],
+    #   [for node in module.cluster_2.cluster_nodes : "${node.public_ip}/32"]
+    # )
+  }
+  ingress {
+    protocol  = "udp"
+    from_port = 4490
+    to_port   = 4490
+    cidr_blocks = ["172.31.0.0/16"]
+  }
+  ingress {
+    protocol  = "udp"
+    from_port = 4800
+    to_port   = 4800
+    cidr_blocks = ["172.31.0.0/16"]
+  }
 }
