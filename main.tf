4a1d69a9456ad272ba3593803413b53d30476a08
Michael Mercier <michael.mercier@ryax.tech>
2024-03-18 17:15:19 +0100

    Working version for basic cluster with network

--- main.tf
+++ main.tf
@@ -1,21 +1,30 @@
 provider "aws" {
-  region = var.region
+  region = "eu-west-3"
+}
+
+locals {
+  instance_type = "i4i.large"
+  ami           = "ami-0cf8088cd49029d99"
 }
 
 module "cluster_1" {
-  source = "./terraform-aws-kubeadm"
+  source = "github.com/RyaxTech/terraform-aws-kubeadm"
 
-  cluster_name         = var.cluster_names[0]
-  master_instance_type = var.instance_type
-  worker_instance_type = var.instance_type
-  num_workers          = 1
+  cluster_name           = "aces-1"
+  master_instance_type   = local.instance_type
+  worker_instance_type   = local.instance_type
+  num_workers            = 1
+  pod_network_cidr_block = "10.42.0.0/16"
+  service_cidr_block     = "10.43.0.0/16"
 }
 
 module "cluster_2" {
-  source = "./terraform-aws-kubeadm"
+  source = "github.com/RyaxTech/terraform-aws-kubeadm"
 
-  cluster_name         = var.cluster_names[1]
-  master_instance_type = var.instance_type
-  worker_instance_type = var.instance_type
-  num_workers          = 1
+  cluster_name           = "aces-2"
+  master_instance_type   = local.instance_type
+  worker_instance_type   = local.instance_type
+  num_workers            = 1
+  pod_network_cidr_block = "10.0.0.0/16"
+  service_cidr_block     = "10.1.0.0/16"
 }
--- terraform.tfvars
+++
@@ -1,9 +0,0 @@
-cluster_names = [
-  "aces-1",
-  "aces-2",
-]
-
-region = "eu-west-3"
-
-instance_type = "i4i.large"
-
